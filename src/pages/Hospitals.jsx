b8f39c195aaa49acb50b097aea3d55d922a9c8db
Priya Kumari <dbgpriyakumari@gmail.com>
2023-03-20 13:56:49 +0530

    card modified

--- src/pages/Hospitals.jsx
+++ src/pages/Hospitals.jsx
@@ -1,5 +1,5 @@
-import Nav from "../components/nav/Nav";
-import "./index.css";
+/*import Nav from "../components/nav/Nav";
+import "./Hospitals.css";
 
 const Hospitals = () => {
     const arr = [{
@@ -21,15 +21,17 @@ const Hospitals = () => {
     return (
         <>
             <Nav />
-            {/* <div></div> searchbox dal dena */}
+            {/* <div></div> searchbox dal dena *//*}
+
+            
             <div className="hospital-card-container">
             {arr.map((ele)=>{
                 return(
                 <div className="card">
                     <img src={ele.pic} alt="" height="80px"/>
                     <div>
-                        {ele.Name}
-                        {ele.Address}
+                        <h2>{ele.Name}</h2>
+                        <p>{ele.Address}</p>
                     </div>
                 </div>
                 )
@@ -38,5 +40,53 @@ const Hospitals = () => {
         </>
     )
 }
+
+export default Hospitals;
+
+*/
+
+/*new code*/
+
+import Nav from "../components/nav/Nav";
+import "./Hospitals.css";
+
+const Hospitals = () => {
+    const arr = [
+        {
+            Name: "asdf",
+            Address: "12 rd new delhi",
+            pic: "https://picsum.photos/200/300"
+        },
+        {
+            Name: "asdf",
+            Address: "12 rd new delhi",
+            pic: "https://picsum.photos/200/300"
+        },
+        {
+            Name: "asdf",
+            Address: "12 rd new delhi",
+            pic: "https://picsum.photos/200/300"
+        }
+    ];
+    return (
+        <>
+            <Nav />
+            {/* <div></div> searchbox dal dena */}
+            <div className="hospital-card-container">
+                {arr.map((ele) => {
+                    return (
+                        <div className="card">
+                            <img src={ele.pic} alt="" height="80px" />
+                            <div className="card-info">
+                                <h2>{ele.Name}</h2>
+                                <p>{ele.Address}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+};
 
 export default Hospitals;
